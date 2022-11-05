3ccef3fd057cf0186c29c90e95915470547d1a1d
Galaxy159 <braun.asaf@gmail.com>
2022-11-05 08:02:19 +0200

    mobile

---
+++ script.js
@@ -0,0 +1,22 @@
+"use-strict";
+
+const questions = document.querySelectorAll(".card__list--item-question");
+
+const arrows = document.querySelectorAll(".card__list--item-arrow");
+
+const answers = document.querySelectorAll(".card__list--item-answer-text");
+
+const previous = [];
+
+questions.forEach((q, index) => {
+  q.addEventListener("click", () => {
+    previous.push(index);
+    arrows[index].classList.toggle("spin");
+    answers[index].classList.toggle("show");
+
+    if (previous.length > 1) {
+      arrows[previous[previous.length - 2]].classList.toggle("spin");
+      answers[previous[previous.length - 2]].classList.toggle("show");
+    }
+  });
+});
